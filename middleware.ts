b848eedf2d25314b32a3de88ca60317eb2334a16
Jignesh Patgir <freshcodetechnology@gmail.com>
2023-09-04 11:09:38 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname == "/ServerError") return NextResponse.next();
-  if (
-    request.cookies.get("jwt_token")?.value &&
-    request.nextUrl.pathname === "/"
-  ) {
-    if (request.cookies.get("role")?.value == "1") {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    } else if (request.cookies.get("role")?.value == "2") {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    } else {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
-  }
-  if (request.cookies.get("jwt_token")?.value) {
-    return NextResponse.next();
-  }
+  // if (request.nextUrl.pathname == "/ServerError") return NextResponse.next();
+  // if (
+  //   request.cookies.get("jwt_token")?.value &&
+  //   request.nextUrl.pathname === "/"
+  // ) {
+  //   if (request.cookies.get("role")?.value == "1") {
+  //     return NextResponse.redirect(new URL("/dashboard", request.url));
+  //   } else if (request.cookies.get("role")?.value == "2") {
+  //     return NextResponse.redirect(new URL("/dashboard", request.url));
+  //   } else {
+  //     return NextResponse.redirect(new URL("/dashboard", request.url));
+  //   }
+  // }
+  // if (request.cookies.get("jwt_token")?.value) {
+  //   return NextResponse.next();
+  // }
   return NextResponse.redirect(new URL("/userlogin", request.url));
 }
 
 export const config = {
   matcher: [
     "/!userlogin",
-    "/!cprofile",
-    "/!dashboard",
+    // "/!cprofile",
+    // "/!dashboard",
     "/!signup",
     "/!resetpassword",
-    "/!companyregister",
+    // "/!companyregister",
     "/!api",
   ],
 };
